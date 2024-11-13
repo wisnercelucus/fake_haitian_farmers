5ab496c0510e08f4e248bc1ce0d343777972b819
wisner celucus <wisnercelicus@gmail.com>
2024-11-13 12:05:11 -0500

    add postgresql

---
+++ core/config/dbsecret_exaple.py
@@ -0,0 +1,5 @@
+DB_HOST="localhost"
+DB_NAME="your_database_name"
+DB_USER="postgres"
+DB_PASSWORD="yourpassword"
+DB_PORT='5432'
--- core/models/Activity.py
+++ core/models/Activity.py
@@ -1,6 +1,17 @@
 from pydantic import BaseModel
 from datetime import date
 from typing import Optional
+from enum import Enum
+
+
+class TypeOfActivity(str, Enum):
+    capacity_building = "Formation/renforcement des capacités"
+    distrib_intrant = "Distribution d'intrants"
+    reunion_de_plaidoyer = "Réunion de plaidoyer"
+    liaison_avec_le_marche = "Liaison avec le marché"
+    autre="Autre"
+
+
 
 class Activity(BaseModel):
     id: int
@@ -11,7 +22,11 @@ class Activity(BaseModel):
     total_male_participant: Optional[int]=None
     total_female_participant: Optional[int]=None
     total_youth_participant: Optional[int]=None
+    description: Optional[str]=None
     date: date
+
+    #Formation/
+    #
 
 
 if __name__ == '__main__':
